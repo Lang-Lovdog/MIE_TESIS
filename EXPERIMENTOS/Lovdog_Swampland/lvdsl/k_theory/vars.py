8b858897b62f6ce022f00448e71dd3487c17f0ae
Lang-Lovdog <brandon.mozart@gmail.com>
2026-04-16 17:59:27 -0600

    cosa

--- EXPERIMENTOS/Lovdog_Swampland/lvdsl/k_theory/vars.py
+++ EXPERIMENTOS/Lovdog_Swampland/lvdsl/k_theory/vars.py
@@ -3,8 +3,11 @@
 ### de Cesar Damian y Oscar Loaiza-Brito
 ### Acceso al experimento original https://doi.org/10.1140/epjc/s10052-022-11118-x
 
+import sys
 ## IMPORTS
 import sympy             as sp  #type: ignore
+from   sympy             import lambdify
+from   xp                import HAS_GPU
 ## IMPORTS
 
 ##### Opciones globales
@@ -33,19 +36,65 @@ variables_numericas={
 ##### Diccionario de valores numéricos globales
 
 # Función potencial V
-V         =  (AH3*s/(tau**3)) + (AF3/(s*tau**3)) + (AF5/(tau**4)) + (A3N3/(tau**3))
-V_lifting =  (AH3*s/(tau**3)) + (AF3/(s*tau**3)) + (AF5/(tau**4)) + (A3N3/(tau**3)) + (AD5/((s**sp.Rational(1,2)*(tau**sp.Rational(5,2)))))
+V                  =  (AH3*s/(tau**3)) + (AF3/(s*tau**3)) + (AF5/(tau**4)) + (A3N3/(tau**3))
+V_lifting          =  (AH3*s/(tau**3)) + (AF3/(s*tau**3)) + (AF5/(tau**4)) + (A3N3/(tau**3)) + (AD5/((s**sp.Rational(1,2)*(tau**sp.Rational(5,2)))))
 # Función potencial V
 
 # Matriz Hessiana de la función potencial
-VHess         = sp.hessian(V, [s,tau])
-V_liftingHess = sp.hessian(V_lifting, [s,tau])
+VHess              = sp.hessian(V, [s,tau])
+V_liftingHess      = sp.hessian(V_lifting, [s,tau])
 # Matriz Hessiana de la función potencial
 
 # Autovalores de VHess
-VHess_eig         = VHess.eigenvals()
-V_liftingHess_eig = V_liftingHess.eigenvals()
+VHess_eig          = list(VHess.eigenvals().keys())
+V_liftingHess_eig  = list(V_liftingHess.eigenvals().keys())
 # Autovalores de VHess
+
+# Traza de hessiana
+V_Hess_tr          = VHess.trace()
+V_liftingHess_tr   = V_liftingHess.trace()
+# Traza de hessiana
+
+# Gradiente del potencial
+V_gradient         = sp.derive_by_array(V, [s,tau])
+V_lifting_gradient = sp.derive_by_array(V_lifting, [s,tau])
+# Gradiente del potencial
+
+# Determinante de la Hessiana
+V_Hess_det         = VHess.det()
+V_liftingHess_det  = V_liftingHess.det()
+# Determinante de la Hessiana
+
+##### Lambdificación de las funciones base
+### ELEMENTOS SIMBÓLICOS ###
+VARIABLES_LAMBDIFICABLES_LIFT  =(AH3,AF3,AF5,A3N3,AD5,tau,s)
+VARIABLES_LAMBDIFICABLES_NOLIFT=(AH3,AF3,AF5,A3N3,    tau,s)
+METHOD = "cupy" if HAS_GPU else "numpy"
+### ELEMENTOS SIMBÓLICOS ###
+### POTENCIAL EFECTIVO ###
+V_lambda                   = lambdify(VARIABLES_LAMBDIFICABLES_NOLIFT, V                 , METHOD)
+V_lifting_lambda           = lambdify(VARIABLES_LAMBDIFICABLES_LIFT  , V_lifting         , METHOD)
+### POTENCIAL EFECTIVO ###
+### HESSIANA ###
+VHess_lambda               = lambdify(VARIABLES_LAMBDIFICABLES_NOLIFT, VHess             , METHOD)
+V_liftingHess_lambda       = lambdify(VARIABLES_LAMBDIFICABLES_LIFT  , V_liftingHess     , METHOD)
+### HESSIANA ###
+### AUTOVALORES ###
+VHess_eig_lambda           = lambdify(VARIABLES_LAMBDIFICABLES_NOLIFT, VHess_eig         , METHOD)
+V_liftingHess_eig_lambda   = lambdify(VARIABLES_LAMBDIFICABLES_LIFT  , V_liftingHess_eig , METHOD)
+### AUTOVALORES ###
+### TRAZA ###
+V_Hess_tr_lambda           = lambdify(VARIABLES_LAMBDIFICABLES_NOLIFT, V_Hess_tr         , METHOD)
+V_liftingHess_tr_lambda    = lambdify(VARIABLES_LAMBDIFICABLES_LIFT  , V_liftingHess_tr  , METHOD)
+### TRAZA ###
+### GRADIENTE ###
+V_gradient_lambda          = lambdify(VARIABLES_LAMBDIFICABLES_NOLIFT, V_gradient        , METHOD)
+V_lifting_gradient_lambda  = lambdify(VARIABLES_LAMBDIFICABLES_LIFT  , V_lifting_gradient, METHOD)
+### GRADIENTE ###
+### DETERMINANTE ###
+V_Hess_det_lambda          = lambdify(VARIABLES_LAMBDIFICABLES_NOLIFT, V_Hess_det        , METHOD)
+V_liftingHess_det_lambda   = lambdify(VARIABLES_LAMBDIFICABLES_LIFT  , V_liftingHess_det , METHOD)
+### DETERMINANTE ###
 
 ##### Definición de valores fijos para búsqueda
 def set_fixed_value(values : dict):
@@ -59,3 +108,18 @@ def set_precision_decimal(val : int):
 
 def get_precision_decimal():
     return precision_decimal
+
+if __name__ == '__main__':
+    sp.init_printing(use_unicode=True)
+    ## Imprimir versión de python
+    print(f"Version de Python: {sys.version}\n")
+    print("Función potencial: V = ")
+    sp.pprint(V)
+    print("Función potencial: V_lifting = ")
+    sp.pprint(V_lifting)
+    print("\nMatriz Hessiana: VHess = ")
+    sp.pprint(VHess)
+    print("\nMatriz Hessiana: V_liftingHess = ")
+    sp.pprint(V_liftingHess)
+    print("\n")
+
---
+++ EXPERIMENTOS/Lovdog_Swampland/lvdsl/xp.py
@@ -0,0 +1,33 @@
+import os
+import sys
+
+xp=None
+np=None
+HAS_GPU = True
+
+def get_nvidia_configs():
+    ## Obtiene la información del entorno virtual en el que se está corriendo
+    venv_path = os.environ.get('VIRTUAL_ENV')
+    if venv_path:
+        cuda_lib_path = os.path.join(venv_path, 'lib', f'python{sys.version_info.major}.{sys.version_info.minor}', 'site-packages', 'nvidia')
+        if os.path.exists(cuda_lib_path):
+            os.environ['CUDA_PATH'] = cuda_lib_path
+            # Añadimos las subcarpetas de nvrtc y cuda_runtime al PATH del sistema
+            os.environ['LD_LIBRARY_PATH'] = f"{cuda_lib_path}/cuda_runtime/lib:{cuda_lib_path}/nvrtc/lib:{cuda_lib_path}:{os.environ.get('LD_LIBRARY_PATH', '')}"
+    else:
+        print("No se encontro ninguna virtual environment activa.")
+        print("Por favor, configure una virtual environment antes de ejecutar este script.")
+        sys.exit(1)
+
+def import_numeric_handle():
+    global np, xp, HAS_GPU
+    import numpy
+    try:
+        import cupy
+        HAS_GPU = True
+        xp = cupy
+        np = numpy
+    except ImportError:
+        print("No se pudo importar cupy. Usando numpy.")
+        np    = numpy
+        xp    = np
